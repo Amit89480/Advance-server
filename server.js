5c418246359ced9065fb6a4fe96e04e18a6be0f0
Amit Pandey <pamit7407084@gmail.com>
2023-12-10 22:14:42 +0530

    script changes done

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "OK" });
+  res.status(200).json({ message: "message from root" });
 });
 
 app.listen(3000, () => console.log("Server is running on port 3000..."));
